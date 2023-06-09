b0f1481be75e7bd3fd7a27f3f6ed86c3ff44bd2d
faisalhatila <hatilas1994@gmail.com>
2023-06-09 20:38:05 +0500

    flutter initial project created

---
+++ lib/map_pratise.dart
@@ -0,0 +1,18 @@
+main() {
+  // var map_name = {
+  //   'Key1': "Value1",
+  //   'Key2': 2,
+  //   'Key3': 3.0,
+  //   'Key4': true,
+  // };
+  // print(map_name['key2']);
+  // var mapName = new Map();
+  // mapName["Name"] = "Faisal";
+  // print("$mapName");
+}
+
+class MyClass {
+  getValue(a, b) {
+    return a + b;
+  }
+}
