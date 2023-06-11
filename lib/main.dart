0e83138bb91f9ce01101c022d4b4b986c22a494a
faisalhatila <hatilas1994@gmail.com>
2023-06-12 03:22:02 +0500

    starting login app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Row(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
