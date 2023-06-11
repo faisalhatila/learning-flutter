c0d535163fcc4ec78a2cbfbfddac3f56374fb7c0
faisalhatila <hatilas1994@gmail.com>
2023-06-12 04:42:32 +0500

    understanding of themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/src/utils/theme/theme.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(const App());
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
+      home: AppHome(),
+    );
+  }
+}
+
+class AppHome extends StatelessWidget {
+  const AppHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        title: Text("Welcome"),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Container(),
-          Container(),
-          Container(),
-        ],
-      ),
+          title: const Text(".appable/"),
+          leading: const Icon(Icons.ondemand_video)),
       floatingActionButton: FloatingActionButton(
-        child: Text("Click"),
-        onPressed: () => print("Hello World"),
-        backgroundColor: Colors.red[600],
+          child: const Icon(Icons.add_shopping_cart_outlined),
+          onPressed: () => {}),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            Text(
+              'Heading',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            Text(
+              'Sub-heading',
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+            Text(
+              'Paragraph',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            ElevatedButton(
+                onPressed: () => {}, child: const Text("Elevated Button")),
+            OutlinedButton(
+                onPressed: () => {}, child: const Text("Outlined Button")),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Image(image: AssetImage("assets/images/books.png")),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/src/utils/theme/widget_themes/text_theme.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme: TTextTheme.lightTextTheme,
+      elevatedButtonTheme:
+          ElevatedButtonThemeData(style: ElevatedButton.styleFrom()));
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
