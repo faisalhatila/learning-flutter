0cbb0c382c93e178d8a58e9960a7fcaa9d9c6767
faisalhatila <hatilas1994@gmail.com>
2023-06-15 06:55:07 +0500

    animation with login screen done

--- lib/src/features/authentication/screens/welcome/welcome_screen.dart
+++ lib/src/features/authentication/screens/welcome/welcome_screen.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:my_first_app/src/common_widgets/fade_in_animation/animation_design.dart';
+import 'package:my_first_app/src/common_widgets/fade_in_animation/fade_in_animation_model.dart';
+import 'package:my_first_app/src/common_widgets/fade_in_animation/fadein_animation_controller.dart';
 import 'package:my_first_app/src/constants/colors.dart';
 import 'package:my_first_app/src/constants/image_strings.dart';
 import 'package:my_first_app/src/constants/sizes.dart';
@@ -11,62 +16,82 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(FadeInAnimationController());
+    controller.startAnimation();
     var mediaQuery = MediaQuery.of(context);
     var brightness = mediaQuery.platformBrightness;
     var height = mediaQuery.size.height;
     final isDarkMode = brightness == Brightness.dark;
     return Scaffold(
       backgroundColor: isDarkMode ? tSecondaryColor : tPrimaryColor,
-      body: Container(
-        padding: EdgeInsets.all(tDefaultSize),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Hero(
-              tag: 'welcome-image-tag',
-              child: Image(
-                image: AssetImage(tWelcomeScreenImage),
-                height: height * 0.6,
+      body: Stack(
+        children: [
+          TFadeInAnimation(
+            durationInMs: 1200,
+            animate: TAnimatePosition(
+                bottomAfter: 0,
+                bottomBefore: -100,
+                leftBefore: 0,
+                leftAfter: 0,
+                topAfter: 0,
+                topBefore: 0,
+                rightAfter: 0,
+                rightBefore: 0),
+            width: null,
+            height: null,
+            child: Container(
+              padding: EdgeInsets.all(tDefaultSize),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Hero(
+                    tag: 'welcome-image-tag',
+                    child: Image(
+                      image: AssetImage(tWelcomeScreenImage),
+                      height: height * 0.6,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        tWelcomeTitle,
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text(
+                        tWelcomeSubTitle,
+                        style: Theme.of(context).textTheme.bodyText1,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                          onPressed: () => {},
+                          child: Text(
+                            tLogin.toUpperCase(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () => {},
+                          child: Text(
+                            tSignup.toUpperCase(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
-            Column(
-              children: [
-                Text(
-                  tWelcomeTitle,
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  tWelcomeSubTitle,
-                  style: Theme.of(context).textTheme.bodyText1,
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: OutlinedButton(
-                    onPressed: () => {},
-                    child: Text(
-                      tLogin.toUpperCase(),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => {},
-                    child: Text(
-                      tSignup.toUpperCase(),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
